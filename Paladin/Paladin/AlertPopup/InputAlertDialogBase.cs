40a3c4a2735131f86c725a8d02e9bda9040ade89
Кирилл Щигарев <coder15@web-axioma.ru>
2018-02-02 17:57:35 +0700

    popups!

---
+++ Paladin/Paladin/AlertPopup/InputAlertDialogBase.cs
@@ -0,0 +1,67 @@
+﻿using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Paladin.AlertPopup
+{
+    /// <summary>
+    /// The awesome Transparent Popup Page
+    /// sub-classed from Rg.Plugins.Popup
+    /// Customized for our usecase with
+    /// Generic data type support for the result
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class InputAlertDialogBase<T> : PopupPage
+    {
+        // the awaitable task
+        public Task<T> PageClosedTask
+        {
+            get { return PageClosedTaskCompletionSource.Task; }
+        }
+
+        // the task completion source
+        public TaskCompletionSource<T> PageClosedTaskCompletionSource { get; set; }
+
+        public InputAlertDialogBase(View contentBody)
+        {
+            Content = contentBody;
+
+            this.BackgroundColor = new Color(0, 0, 0, 0.4);
+
+            // init the task completion source
+            PageClosedTaskCompletionSource = new System.Threading.Tasks.TaskCompletionSource<T>();
+        }
+
+        // Method for animation child in PopupPage
+        // Invoced after custom animation end
+        protected override Task OnAppearingAnimationEnd()
+        {
+            return Content.FadeTo(1);
+        }
+
+        // Method for animation child in PopupPage
+        // Invoked before custom animation begin
+        protected override Task OnDisappearingAnimationBegin()
+        {
+            return Content.FadeTo(1);
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            // Prevent back button pressed action on android
+            //return base.OnBackButtonPressed();
+            return true;
+        }
+
+        // Invoced when background is clicked
+        protected override bool OnBackgroundClicked()
+        {
+            // Prevent background clicked action
+            //return base.OnBackgroundClicked();
+            return false;
+        }
+    }
+}
--- Paladin/Paladin/ViewModels/CharacterViewModel.cs
+++ Paladin/Paladin/ViewModels/CharacterViewModel.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Paladin.AlertPopup;
+using Rg.Plugins.Popup.Services;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace Paladin.ViewModels
@@ -30,12 +33,62 @@ namespace Paladin.ViewModels
         }
 
         public Command LoadItemsCommand { get; set; }
+        public ICommand InputPopupCommand { get; }
 
         public CharacterViewModel()
         {
             Title = "Гунтер Громобород";
             ListOfFeats = new List<FeatList>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+            InputPopupCommand = new Command(async (e) => 
+            {
+                var result = await ExecuteInputPopup();
+                Debug.WriteLine("**** ****: " + result);
+            } );
+        }
+
+        private async Task<string> ExecuteInputPopup()
+        {
+            // create the TextInputView
+            var inputView = new TextInputView(
+                "Введите значение:", 
+                "Введите текст...",
+                "Поле ввода не должно быть пустым!",
+                "Ок");
+
+            // create the Transparent Popup Page
+            // of type string since we need a string return
+            var popup = new InputAlertDialogBase<string>(inputView);
+
+            // subscribe to the TextInputView's Button click event
+            inputView.CloseButtonEventHandler +=
+                (sender, obj) =>
+                {
+                    if (!string.IsNullOrEmpty(((TextInputView)sender).TextInputResult))
+                    {
+                        ((TextInputView)sender).IsValidationLabelVisible = false;
+
+                        // update the page completion source
+                        popup.PageClosedTaskCompletionSource.SetResult(((TextInputView)sender).TextInputResult);
+                    }
+                    else
+                    {
+                        ((TextInputView)sender).IsValidationLabelVisible = true;
+                    }
+                };
+
+            // Push the page to Navigation Stack
+            await PopupNavigation.PushAsync(popup);
+
+            // await for the user to enter the text input
+            var result = await popup.PageClosedTask;
+
+            // Pop the page from Navigation Stack
+            await PopupNavigation.PopAsync();
+
+            // return user inserted text value
+            return result;
         }
 
         private async Task ExecuteLoadItemsCommand()
@@ -61,9 +114,17 @@ namespace Paladin.ViewModels
 
                 var clericList = new FeatList()
                 {
-                    new Feat() { Title = "Канал света", Decription = "2/2" }
+                    new Feat() { Title = "Изгнание нежити", Decription = "2/2" },
+                    new Feat() { Title = "Разрушительный гнев", Decription = "2/2" }
                 };
-                clericList.Heading = "Классовые способности:";
+                clericList.Heading = "Божественный канал:";
+
+                var domainList = new FeatList()
+                {
+                    new Feat() { Title = "Гнев бури (реакция), спас Ловк. 2к8/пол.", Decription = "Муд." },
+                    new Feat() { Title = "Удар грома, толчок 10 футов", Decription = "-" }
+                };
+                domainList.Heading = "Домен бури:";
 
                 var healingList = new FeatList()
                 {
@@ -75,6 +136,7 @@ namespace Paladin.ViewModels
                 {
                     slotList,
                     clericList,
+                    domainList,
                     healingList
                 };
 
