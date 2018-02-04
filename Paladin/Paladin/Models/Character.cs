6ac5b83deaf1e8afb5e6ea0ea42c0492e02cdefd
Щигарев Кирилл <kirill.shchigarev@gmail.com>
2018-02-05 02:39:48 +0300

    try from home

---
+++ Paladin/Paladin/Models/Character.cs
@@ -0,0 +1,45 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paladin.Models
+{
+    [Table("Characters")]
+    public class Character
+    {
+        [SQLite.PrimaryKey, SQLite.AutoIncrement, SQLite.Column("_id")]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int HP { get; set; }
+        public int TemporalHP { get; set; }
+        public int HitDices { get; set; }
+        public Classes Class { get; set; }
+        public List<Slot> Slots { get; set; }
+    }
+
+    public enum Classes
+    {
+        Barbarian,
+        Bard,
+        Cleric,
+        Druid,
+        Warrior,
+        Monk,
+        Paladin,
+        Ranger,
+        Rogue,
+        Sorcerer,
+        Warlock,
+        Wizzard
+    }
+
+    public class Slot
+    {
+        public string Title { get; set; }
+        public int Amount { get; set; }
+    }
+
+}
---
+++ Paladin/Paladin/Services/CreateCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paladin.Services
+{
+    public class CreateCharacter
+    {
+        public static async Task NewCleric()
+        {
+
+        }
+    }
+}
